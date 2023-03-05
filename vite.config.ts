c27e1bd296781b9e856b24f4af110c3af1625a62
wyx <2603243185@qq.com>
2023-03-05 13:27:52 +0800

    vite打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
+import commpressPlugin from 'vite-plugin-compression'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    commpressPlugin({
+      verbose: true, // 默认即可
+      disable: false, //开启压缩(不禁用)，默认即可
+      deleteOriginFile: false, //删除源文件
+      threshold: 10240, //压缩前最小文件大小
+      algorithm: 'gzip', //压缩算法
+      ext: '.gz' //文件类型
+    })
+  ],
+  esbuild: {
+    pure: ['console.log']
+  },
+  build: {
+    minify: true
+  },
   css: {
     preprocessorOptions: {
       scss: {
